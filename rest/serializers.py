d7dd456a23593d8a52ae5a53454173d3b8316f95
Anton Stupak <s2pak.anton@gmail.com>
2015-07-10 13:50:03 +0300

    Fix serializer.

--- rest/serializers.py
+++ rest/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from rest.models import Score
 
-class TrackingLogSerializer(serializers.ModelSerializer):
+class ScoreSerializer(serializers.ModelSerializer):
     class Meta:
         model = Score
         fields = ("id", "points_possible", "points_earned", "user_id", "course_id", "usage_id")
--- rest/views.py
+++ rest/views.py
@@ -1,8 +1,8 @@
 from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest.helpers import get_course_policy, get_course_structure
-from rest.serializers import TrackingLogSerializer
+from rest.helpers import get_course_policy, get_course_structure, get_scores
+from rest.serializers import ScoreSerializer
 
 
 class ScoreView(APIView):
@@ -15,9 +15,8 @@ class ScoreView(APIView):
 
     @staticmethod
     def post(request, format=None):
-        serializer = TrackingLogSerializer(data=request.data)
+        serializer = ScoreSerializer(data=request.data)
         if serializer.is_valid():
-
             serializer.save()
             return Response(serializer.data, status=201)
         return Response(serializer.errors, status=400)
@@ -25,9 +24,11 @@ class ScoreView(APIView):
     @staticmethod
     def get(request):
         course_id = request.query_params.get('course_id', None)
-        if course_id:
+        user_id = request.query_params.get('user_id', None)
+        if course_id and user_id:
             # TODO: I don't know what to do with jsons: grading_policy, course_structure
             grading_policy = get_course_structure(course_id)
             course_structure = get_course_policy(course_id)
+            scores = get_scores(course_id, user_id)
             return Response({'status': 'success'}, status=200)
-        return Response({'error': 'Course_id is None'}, status=400)
+        return Response({'error': 'course_id or user_id is None'}, status=400)
