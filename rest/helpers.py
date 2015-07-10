a8f9f94193d8dd36435a2fd4243f38fe92c52a85
Anton Stupak <s2pak.anton@gmail.com>
2015-07-10 14:02:39 +0300

    Update helpers.

--- rest/helpers.py
+++ rest/helpers.py
@@ -6,28 +6,29 @@ from requests.exceptions import RequestException
 from rest.models import Score
 
 
-def get_course_api(url, course_id):
+def send_request(url, params=None):
     """
     :param course_id: sting
     :param url: string (Course structure API endpoint)
     :return: Api request to url with param course_id
     """
-    url = urlparse.urljoin(url, course_id)
     try:
-        response = requests.get(url=url)
+        response = requests.get(url=url, params=params)
     except RequestException:
         raise RequestException
-    return json.loads(response.text)
+    return response.json()
 
 
 def get_course_structure(course_id):
-    url = urlparse.urljoin(settings.COURSE_STRUCTURE_API, '/course_structures/')
-    get_course_api(url, course_id)
+    FIELDS = ['has_score', 'weight', 'children', 'graded', 'format', 'always_recalculate_grades', 'max_score']
+    url = urlparse.urljoin(settings.COURSE_STRUCTURE_API, '/courses/{course_id}/blocks/'.format(course_id=course_id))
+    params = {'fields': ','.join(FIELDS)}
+    return send_request(url, params)
 
 
 def get_course_policy(course_id):
-    url = urlparse.urljoin(settings.COURSE_STRUCTURE_API, '/grading_policies/')
-    get_course_api(url, course_id)
+    url = urlparse.urljoin(settings.COURSE_STRUCTURE_API, '/grading_policies/{course_id}/'.format(course_id=course_id))
+    return send_request(url)
 
 
 def get_scores(course_id, user_id):
